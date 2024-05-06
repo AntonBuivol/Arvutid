e5daa254fdb226ac5b4b596d3000ecdb5b56c52b
Luca <luca.gluhhov@gmail.com>
2024-05-06 11:14:10 +0300

    tegin 1 ülesanne  Vahetasin lehel nimega ControllPage nuppud Sissepääs ja Registrerimine

--- TheFinalProj/ControllPage.php
+++ TheFinalProj/ControllPage.php
@@ -10,13 +10,13 @@
 Tere tulemast! Taotluse jätmiseks minge veebisaidile.
 </h1>
  <div class="Login">
-        <a href="login.php">
-            <button>Sissepääs</button>
+        <a href="register.php">
+            <button>registreerimine</button>
         </a>
     </div>
 <div class="Reg">
-    <a href="register.php">
-        <button>Registreerimine</button>
+    <a href="login.php">
+        <button>Sissepääs</button>
     </a>
 </div>
 </body>
